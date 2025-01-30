c2a19a8474a709072286b061d5c469c1e783715f
majawojtowicz <wojtowiczmagdalenamaja@gmail.com>
2025-01-30 20:22:43 +0100

    Update dispatcher.c

--- dispatcher.c
+++ dispatcher.c
@@ -5,18 +5,18 @@ extern volatile sig_atomic_t noMoreCheckIn;
 
 void *dispatcher_thread(void *arg)
 {
-    DispatcherParams *dp = (DispatcherParams*)arg;
+    DispatcherParams *dp = ( DispatcherParams*)arg;
 
     printf("[DISPATCHER] Thread started. planesCount=%d\n", dp->planesCount);
 
     
     int forced = 0;
     while (forced < 2) {
-        sleep(5);
+    sleep( 5 );
         printf("[DISPATCHER] Sending SIGUSR1 (force_departure)\n");
-        kill(getpid(), SIGUSR1);
+    kill( getpid(), SIGUSR1);
         forced++;
-    }
+}
 
     sleep(5);
     printf("[DISPATCHER] Sending SIGUSR2 => no more check-in.\n");
